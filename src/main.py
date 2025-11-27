2ff2fd2efeefd900c0f7cff7310c3517a96cd4c3
Orolau <lau.ap04@gmail.com>
2025-11-27 12:26:13 +0100

    Estructura completa main

--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ from modules.ia_module import get_related_company_names
 from modules.whois_module import get_whois_all_domains
 from modules.reports_module import generar_informe_pdf
 from modules.locations_module import get_company_locations_serpapi
+from modules.employees_module import get_company_employees_serpapi
+from modules.shodan_module import shodan_get_assets
 
 def main():
     db = connect_database()
-    generar_informe_pdf("mercadona", db, "20251108")
-    """company = input("Introduce el nombre de la empresa: ")
+    company = input("Introduce el nombre de la empresa: ")
     get_related_company_names(company, db)
     get_crtsh_and_classify(company, db)
     get_domains_WHOISXMLAPI(company, db)
@@ -17,7 +18,11 @@ def main():
     get_subdomains(company, db)
     get_whois_all_domains(company, db)
     get_asn_info(company, db)
-    get_company_locations_serpapi(company, db)"""
+    shodan_get_assets(company, db)
+    get_company_locations_serpapi(company, db)
+    get_company_employees_serpapi(company, db)
+    generar_informe_pdf(company, db)
+    
 
 if __name__ == "__main__":
     main()
